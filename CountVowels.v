7b0a5f4e0fcf93e143dc35e295cf2f949bf2dae4
zach <zach@dylibso.com>
2023-07-27 17:35:39 -0700

    cleanup: add comments to coq code

--- CountVowels.v
+++ CountVowels.v
@@ -3,6 +3,8 @@ Require Import Ascii.
 Require Import Init.Byte.
 
 Open Scope byte_scope.
+
+(* Check if a byte represents a vowel character *)
 Definition is_vowel (b: byte) :=
   match b with
     | "A" | "a" | "E" | "e" | "I" | "i" | "O" | "o" | "U" | "u" => true
@@ -10,6 +12,8 @@ Definition is_vowel (b: byte) :=
     end.
 
 Open Scope string_scope.
+
+(* Count the number of vowels in a string *)
 Fixpoint count_vowels (s: string) : nat :=
   match s with
   | "" => 0
@@ -19,17 +23,20 @@ Fixpoint count_vowels (s: string) : nat :=
   end.
 
 
+(* Sanity check*)
+Example count_vowels_test: count_vowels "this is a test" = 4.
+Proof.
+  reflexivity.
+Qed.
+
+(* Verify that an empty string has 0 vowels *)
 Example count_vowels_empty_0: count_vowels "" = 0.
 Proof.
   unfold count_vowels.
   reflexivity.
 Qed.
 
-Example count_vowels_test: count_vowels "this is a test" = 4.
-Proof.
-  reflexivity.
-Qed.
-
+(* Verify that adding a vowel to a string increases the vowel count by 1 *)
 Example count_vowels_plus_vowel: forall x c, is_vowel c = true -> count_vowels (String (ascii_of_byte c) x) = count_vowels x + 1.
 Proof.
   intros.
@@ -37,8 +44,6 @@ Proof.
   - unfold count_vowels. rewrite byte_of_ascii_of_byte. rewrite H. reflexivity.
   - simpl. rewrite byte_of_ascii_of_byte. rewrite H. reflexivity. 
 Qed.
-
-Eval compute in count_vowels "abcdefghi".
 
 Require Coq.extraction.Extraction.
 Require Import ExtrHaskellBasic.
